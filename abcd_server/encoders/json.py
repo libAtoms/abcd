59cac6d76d124f17704852af1b86959035e08bd9
Adam Fekete <adam.fekete@kcl.ac.uk>
2019-01-21 19:31:15 +0000

    cleaning up the json encoder

--- abcd_server/encoders/json.py
+++ abcd_server/encoders/json.py
@@ -1,6 +1,6 @@
 import json
 import base64
-from .base import BaseEncoder
+from abcd_server.encoders.base import BaseEncoder
 import numpy as np
 from ase import Atoms
 from ase.calculators.singlepoint import SinglePointCalculator
@@ -118,3 +118,75 @@ class JSONDecoderOld(json.JSONDecoder):
             )
         )
         return atoms
+
+
+if __name__ == '__main__':
+
+    from ase.io import iread
+    from pprint import pprint
+
+    for atoms in iread('../../utils/data/bcc_bulk_54_expanded_2_high.xyz', index=slice(None)):
+        # print(at)
+        atoms.calc.results['forces'] = atoms.arrays['force']
+        print(atoms)
+
+        json_data = json.dumps(atoms, cls=JSONEncoderOld)
+        print(json_data)
+
+        atom_dict = json.loads(json_data, cls=JSONDecoderOld)
+        print(atom_dict)
+
+        print(atoms == atom_dict)
+
+    with JSONEncoder() as encoder:
+        data = encoder.encode(atoms)
+
+    print(data)
+
+# class Visitor(object):
+#     def __init__(self, atoms, visitor):
+#         self.atoms = atoms
+#         self.visitor = visitor
+#
+#     def visit(self):
+#         return self.visitor.visit_atoms(self.atoms)
+#
+# visitor = Visitor(at, DictEncoder())
+# visitor = Visitor(at, JSONEncoder())
+# pprint(visitor.visit())
+
+
+# class DictConverter(object):
+#     default_properties = ['numbers', 'positions']
+#
+#     def encode(self, atoms):
+#         data = {
+#             # 'formula': obj.get_chemical_formula(),
+#             'numbers': atoms.numbers.tolist(),
+#             'pbc': atoms.pbc.tolist(),
+#             'cell': atoms.cell,
+#             'positions': atoms.positions,
+#             'forces': atoms.get_forces(),
+#             'energy': atoms.get_potential_energy(),
+#             'metadata': {}
+#         }
+#         for key, val in atoms.arrays.items():
+#             if key not in self.default_properties:
+#                 data['metadata'][key] = val
+#
+#         return data
+#
+#     def decode(self, data):
+#         atoms = Atoms(
+#             numbers=data.get('numbers', None),
+#             pbc=data.get('pbc', None),
+#             cell=data.get('cell', None),
+#             positions=data.get('positions', None),
+#         )
+#         atoms.set_calculator(
+#             SinglePointCalculator(
+#                 atoms,
+#                 forces=data.get('forces', None),
+#                 energy=data.get('energy', None)
+#             )
+#         )

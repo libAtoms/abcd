fd46eed791aaca4cea55a6966203cc31a0d53606
Adam Fekete <adam.fekete@kcl.ac.uk>
2019-01-22 18:58:45 +0000

    first implementation of the frontend page

--- abcd_server/app/views/index.py
+++ abcd_server/app/views/index.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask import jsonify
+import requests
+
+from abcd_server.app.views import api
 
 bp = Blueprint('index', __name__)
 
@@ -25,33 +29,11 @@ def index():
                            title=index_title,
                            content=index_content)
 
-# from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
-# from markupsafe import escape
-# from forms import SignupForm
 
-# from app.nav import nav
-# from database import generate_plot, Item, ItemTable
-# from admin import requires_auth
+@bp.route('/calculation', methods=['GET'])
+def get_calculation():
+    calculations = api.get_calculation().json
+    return render_template("database.html",
+                           title=index_title,
+                           content=calculations)
 
-# @frontend.app_template_filter('reverse')
-# def include_file(name):
-#     return name+'haha'
-
-# # Shows a long signup form, demonstrating form rendering.
-# @frontend.route('/example-form/', methods=('GET', 'POST'))
-# def example_form():
-#     form = SignupForm()
-#
-#     if form.validate_on_submit():
-#         # We don't have anything fancy in our application, so we are just
-#         # flashing a message when a user completes the form successfully.
-#         #
-#         # Note that the default flashed messages rendering allows HTML, so
-#         # we need to escape things if we input user values:
-#         flash('Hello, {}. You have successfully signed up'
-#               .format(escape(form.name.data)))
-#
-#         # In a real application, you may wish to avoid this tedious redirect.
-#         return redirect(url_for('.index'))
-#
-#     return render_template('signup.html', form=form)

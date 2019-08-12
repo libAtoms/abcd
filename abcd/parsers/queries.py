8d98a8b97bf3bac997168c141046b110da25d59b
Adam Fekete <adam.fekete@kcl.ac.uk>
2019-08-12 18:05:55 +0100

    first implementation of query parser

--- abcd/parsers/queries.py
+++ abcd/parsers/queries.py
@@ -8,51 +8,110 @@ logger = logging.getLogger(__name__)
 # https://github.com/Materials-Consortia/optimade-python-tools/tree/master/optimade/grammar
 
 grammar = r"""
-    start: expression |
-    //expression: [ NOT ] NAME  [ OPERATOR value ] [ conjunction expression ]
-    expression: [ NOT ] ( NAME | sum ) [ OPERATOR ( value | sum ) ] [ conjunction expression ] 
-              | [ NOT ] ( value | sum ) [ REVERSED ( NAME | sum ) ] [ conjunction expression ]
+    ?start: expression | 
+    ?expression: (negation | single_expression | reversed_expression | grouped_expression) [ relation expression ] 
 
-    OPERATOR: "=" | "!=" | ">" | ">=" | "<" | "<="| "~" 
-    REVERSED: "in"
+    single_expression:   ( NAME | function ) [ operators value ]
+    reversed_expression: value reversed_operators NAME  
+   
+    grouped_expression:   "(" expression ")"
+    negation: NOT (single_expression | reversed_expression | grouped_expression)
+    
+    ?operators: EQUAL
+             | NOTEQUAL
+             | GT
+             | GTE
+             | LT
+             | LTE
+             | REGEXP
 
-    conjunction: AND | OR 
+    ?reversed_operators: IN
+        
+    EQUAL:    "=" 
+    NOTEQUAL: "!=" 
+    GT:       ">" 
+    GTE:      ">=" 
+    LT:       "<" 
+    LTE:      "<="
+    REGEXP:   "~"     
+    IN:       "in"
+
+    ?relation: AND | OR 
+
     AND: "&" | "and"
-    OR: "|" | "or"
+    OR:  "|" | "or"
     NOT: "!" | "not"
 
-    value: NAME | SIGNED_FLOAT | SIGNED_INT | ESCAPED_STRING 
-    
-    // arithmetic
-    ?sum: product
-        | func
-        | sum "+" product   -> add
-        | sum "-" product   -> sub
-    ?product: atom
-        | product "*" atom  -> mul
-        | product "/" atom  -> div
-    ?atom: NUMBER           -> number
-         | "-" atom         -> neg
-         | NAME             -> var
-         | "(" sum ")"
+    value: NAME 
+         | FLOAT  -> float
+         | INT    -> int
+         | STRING -> string
+         | "True"             -> true
+         | "False"            -> false
+         | array
 
-    // functions
-    func: "all" "(" NAME ")" | "any" "(" NAME ")"
-         
-    %import common.CNAME -> NAME
-    %import common.SIGNED_FLOAT
-    %import common.SIGNED_INT
-    %import common.ESCAPED_STRING
+    array: "[" value ([","] value)* "]"
+    
+    function: all | any
+    
+    all: "all" "(" NAME ")" 
+    any: "any" "(" NAME ")"
+
+     
+    %import common.CNAME          -> NAME
+    %import common.SIGNED_FLOAT   -> FLOAT
+    %import common.SIGNED_INT     -> INT
+    %import common.ESCAPED_STRING -> STRING
     %import common.WS_INLINE
-    %import common.NUMBER
 
     %ignore WS_INLINE
 """
 
 
 class TreeToAST(Transformer):
-    def start(self, s):
+    def start(self, _):
+        return None
+
+    int = v_args(inline=True)(int)
+    float = v_args(inline=True)(float)
+    array = list
+
+    true = lambda self, _: True
+    false = lambda self, _: False
+
+    @v_args(inline=True)
+    def string(self, s):
         return s[1:-1].replace('\\"', '"')
+
+    def single_expression(self, items):
+
+        if len(items) == 1:
+            return {str(items[0]): {'EXISTS': True}}
+
+        if len(items) != 3:
+            raise NotImplementedError
+
+        return {str(items[0]): {items[1].type: items[2]}}
+
+    def reversed_expression(self, items):
+
+        if len(items) != 3:
+            raise NotImplementedError
+
+        return {str(items[2]): {items[1].type: items[0]}}
+
+    def grouped_expression(self, items):
+        return items[0]
+
+    def negation(self, s):
+        return {k: {s[0].type: v} for k, v in s[1].items()}
+
+    def expression(self, items):
+
+        if len(items) != 3:
+            raise NotImplementedError
+
+        return {items[1].type: [items[0], items[2]]}
 
 
 class Parser:
@@ -65,33 +124,42 @@ class Parser:
 
 
 parser = Parser()
+transformer = TreeToAST()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
     queries = (
-        'single'
+        ' ',
+        'single',
+        'not single',
         'operator_gt > 23 ',
+        'operator_gt > -2.31e-5 ',
         'string = "some string"',
         'regexp ~ ".*H"',
         'aa & not bb',
         'aa & bb > 23.54 | cc & dd',
         'aa and bb > 23 and bb > 23 and bb > 23 ',
         'aa and bb > 23.54 or 22 in cc and dd',
-        'expression = (bb/3-1)*cc',
-        'energy/n_atoms > 3',
-        '1=3',
-        'all(aa) > 3',
-        'any(aa) > 3',
+        'aa & bb > 23.54 | (22 in cc & dd)',
+        'aa and bb > 23.54 or (22 in cc and dd)',
+        'aa and not (bb > 23.54 or (22 in cc and dd))',
+        # # 'expression = (bb/3-1)*cc',
+        # # 'energy/n_atoms > 3',
+        # '1=3',
+        # 'all(aa) > 3',
+        # 'any(aa) > 3',
+        # 'aa = False',
         # 'aa = [True True True]',
-        # 'aa & bb > 23.54 | (22 in cc & dd)',
-        # 'aa and bb > 23.54 or (22 in cc and dd)',
-        # 'aa and (bb > 23.54 or (22 in cc and dd))',
     )
 
     for query in queries:
-        print(query)
-        print(parser.parse(query).pretty())
+        # print(query)
 
-    # if '':
-    print(parser.parse(''))
+        # print(parser.parse(query).pretty())
+        try:
+            tree = parser.parse(query)
+            # print('=> tree:', tree)
+            print('==> ast:', transformer.transform(tree))
+        except LarkError:
+            raise NotImplementedError
